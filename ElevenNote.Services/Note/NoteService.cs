317bdefb73539e4a22a798f310ae213a498355fb
Eilers Fam <alexeilers@Alexs-iMac.local>
2022-03-18 10:01:57 -0400

    added delete note

--- ElevenNote.Services/Note/NoteService.cs
+++ ElevenNote.Services/Note/NoteService.cs
@@ -88,5 +88,16 @@ namespace ElevenNote.Services.Note
             return numberOfChanges == 1;
 
         }
+
+        public async Task<bool> DeleteNoteAsync(int noteId)
+        {
+            var noteEntity = await _dbContext.Notes.FindAsync(noteId);
+
+            if (noteEntity?.OwnerId != _userId)
+                return false;
+
+            _dbContext.Notes.Remove(noteEntity);
+            return await _dbContext.SaveChangesAsync() == 1;
+        }
     }
 }
